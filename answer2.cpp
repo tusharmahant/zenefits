8a92dbbd49cbf423f7178005738b6f72bcf5ae41
tusharmahant <38071155+tusharmahant@users.noreply.github.com>
2018-04-05 19:40:02 +0530

    Add files via upload

---
+++ answer2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rankoftheword(int n, vector <char> word(n)){
+    
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector <char> word(n);
+    for(int i = 0; i < n; i++){
+        cin >> word[i];
+        cout << "\n";
+    }
+    string theword;
+    cin >> theword;
+    
+    rankoftheword(n, word);
+	// your code goes here
+	return 0;
+}
